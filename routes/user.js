498c13e620e952acbc3ab5ee25954510bf598ee4
iCode_Rx <arababs2015@gmail.com>
2021-07-25 19:31:51 +0100

    third commit

--- routes/user.js
+++ routes/user.js
@@ -71,7 +71,115 @@ router.delete('/:id', async (req, res) => {
 })
 
 // get a user
+/*
+Register a user
+dn use in userId here
+http://localhost:8800/api/users/id
+e.g: id = 60fda19b8455a0309e48084d
+*/ 
+router.get('/:id', async (req, res)=> {
+          try {
+                    const user = await User.findById(req.params.id)
+                    // return res.status(200).json(user)
+
+                    // to hide some ppties like password do ds
+                    const {password, updatedAt, ...other} = user._doc
+
+                    return res.status(200).json(other)
+
+                    return res.status(200).json(user)
+          } catch (err) {
+                    return res.status(500).json(err)
+          }
+})
+
 // follow a user
+/*
+person to follow:
+http://localhost:8800/api/users/60fda89a9b52a432b3d42104/follow
+
+follower id
+{
+          "userId": "60fda19b8455a0309e48084d"
+}
+*/ 
+router.put("/:id/follow", async (req, res) => {
+          if(req.body.userId !== req.params.id) {
+                    try {
+                              const user = await User.findById(req.params.id)
+
+                              const currentUser = await User.findById(req.body.userId)
+
+                              if(!user.followers.includes(req.body.userId)) {
+                                        await user.updateOne(
+                                                  {
+                                                            $push: {
+                                                                      followers: req.body.userId
+                                                            }
+                                                  }
+                                        )
+
+                                        await currentUser.updateOne(
+                                                  {
+                                                            $push: {
+                                                                      followings: req.params.id
+                                                            }
+                                                  }
+                                        )
+
+                                        return res.status(200).json("user has been followed")
+
+                              }else {
+                                        return res.status(403).json("you already follow this user")
+                              }
+                                        
+
+                    } catch (err) {
+                              return res.status(500).json(err)
+                    }
+          }else {
+                    res.status(403).json("you can't follow yourself")
+          }
+})
+
 // unfollow a user
+router.put("/:id/unfollow", async (req, res) => {
+          if(req.body.userId !== req.params.id) {
+                    try {
+                              const user = await User.findById(req.params.id)
+
+                              const currentUser = await User.findById(req.body.userId)
+
+                              if(!user.followers.includes(req.body.userId)) {
+                                        await user.updateOne(
+                                                  {
+                                                            $push: {
+                                                                      followers: req.body.userId
+                                                            }
+                                                  }
+                                        )
+
+                                        await currentUser.updateOne(
+                                                  {
+                                                            $push: {
+                                                                      followings: req.params.id
+                                                            }
+                                                  }
+                                        )
+
+                                        return res.status(200).json("user has been followed")
+
+                              }else {
+                                        return res.status(403).json("you already follow this user")
+                              }
+                                        
+
+                    } catch (err) {
+                              return res.status(500).json(err)
+                    }
+          }else {
+                    res.status(403).json("you can't follow yourself")
+          }
+})
 
 module.exports = router
