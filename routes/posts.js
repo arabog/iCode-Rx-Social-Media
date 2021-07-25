5a3ffd45f4c38578fd652b9c402bc0b0d724c6cc
iCode_Rx <arababs2015@gmail.com>
2021-07-25 21:16:20 +0100

    sith commit

--- routes/posts.js
+++ routes/posts.js
@@ -22,7 +22,34 @@ router.post('/', async (req, res) => {
 })
 
 // update a post
+/*
+http://localhost:8800/api/posts/id
+http://localhost:8800/api/posts/60fdc2e4a4386a3d945cfaff
 
+{
+          "userId": "60fda89a9b52a432b3d42104",
+          "desc": "Hey my first post has just been updated"
+}
+notice dt d id is different 4rm userId
+u send userId with ur request while "/:id" is d db id
+_id: ObjectId("60fdc2e4a4386a3d945cfaff") 
+
+*/ 
+router.put("/:id", async (req, res) => {
+          try {
+                    const post = await Post.findById(req.params.id)
+
+                    if(post.userId === req.body.userId) {
+                              await post.updateOne({$set: req.body})
+
+                              res.status(200).json("the post has been updated")
+                    }else {
+                              res.status(403).json("you can update only your post")
+                    }
+          } catch (err) {
+                    res.status(500).json(err)
+          }
+})
 
 // del a post
 
