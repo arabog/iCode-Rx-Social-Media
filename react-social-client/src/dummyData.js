adac27fee928197e8f5f6a0a29483d1ad56d5c97
iCode_Rx <arababs2015@gmail.com>
2021-07-27 21:41:32 +0100

    dummyData created

---
+++ react-social-client/src/dummyData.js
@@ -0,0 +1,165 @@
+export const Users = [
+          {
+                    id: 1,
+                    profilePicture: "assets/person/1.jpeg",
+                    username: "Safak Kocaoglu",
+          },
+
+          {
+                    id: 2,
+                    profilePicture: "assets/person/2.jpeg",
+                    username: "Janell Shrum",
+          },
+
+          {
+                    id: 3,
+                    profilePicture: "assets/person/3.jpeg",
+                    username: "Alex Durden",
+          },
+
+          {
+                    id: 4,
+                    profilePicture: "assets/person/4.jpeg",
+                    username: "Dora Hawk",
+          },
+
+          {
+                    id: 5,
+                    profilePicture: "assets/person/5.jpeg",
+                    username: "Thomas Holden",
+          },
+
+          {
+                    id: 6,
+                    profilePicture: "assets/person/6.jpeg",
+                    username: "Shirley Beauchamp",
+          },
+
+          {
+                    id: 7,
+                    profilePicture: "assets/person/7.jpeg",
+                    username: "Travis Bennet",
+          },
+
+          {
+                    id: 8,
+                    profilePicture: "assets/person/8.jpeg",
+                    username: "Kristen Thomas",
+          },
+
+          {
+                    id: 9,
+                    profilePicture: "assets/person/9.jpeg",
+                    username: "Gary Duty",
+          },
+
+          {
+                    id: 10,
+                    profilePicture: "assets/person/10.jpeg",
+                    username: "Safak Olughene",
+          },
+
+]
+
+export const Posts = [
+          {
+                    id: 1,
+                    desc: "Love For All, Hatred  For None",
+                    photo: "assets/post/1.jpeg",
+                    date: "5 mins ago",
+                    userId: 1,
+                    like: 32,
+                    comment: 9,
+          },
+
+          {
+                    id: 2,
+                    desc: "The Hatred You Give, Live ForEver",
+                    photo: "assets/post/2.jpeg",
+                    date: "15 mins ago",
+                    userId: 2,
+                    like: 2,
+                    comment: 1,
+          },
+
+          {
+                    id: 3,
+                    desc: "Health is Wealth and Not other way round",
+                    photo: "assets/post/3.jpeg",
+                    date: "1 mins ago",
+                    userId: 3,
+                    like: 20,
+                    comment: 90,
+          },
+
+          {
+                    id: 4,
+                    desc: "For God So Love The World, He Gave His Only Son, Jesus",
+                    photo: "assets/post/4.jpeg",
+                    date: "5 mins ago",
+                    userId: 4,
+                    like: 320,
+                    comment: 199,
+          },
+
+          {
+                    id: 5,
+                    desc: "He is our advocate and defender",
+                    photo: "assets/post/5.jpeg",
+                    date: "50 mins ago",
+                    userId: 5,
+                    like: 112,
+                    comment: 90,
+          },
+
+          {
+                    id: 6,
+                    desc: "For we are bone of His bone, Flesh of His flesh",
+                    photo: "assets/post/6.jpeg",
+                    date: "35 mins ago",
+                    userId: 6,
+                    like: 3200,
+                    comment: 900,
+          },
+
+          {
+                    id: 7,
+                    desc: "The World needs Jesus",
+                    photo: "assets/post/7.jpeg",
+                    date: "25 mins ago",
+                    userId: 7,
+                    like: 35,
+                    comment: 29,
+          },
+
+          {
+                    id: 8,
+                    desc: "Jesus alone is our message",
+                    photo: "assets/post/8.jpeg",
+                    date: "5 mins ago",
+                    userId: 8,
+                    like: 32,
+                    comment: 9,
+          },
+
+          {
+                    id: 9,
+                    desc: "He is the atonement for out sin",
+                    photo: "assets/post/9.jpeg",
+                    date: "5 mins ago",
+                    userId: 9,
+                    like: 52,
+                    comment: 59,
+          },
+
+          {
+                    id: 10,
+                    desc: "He first loved us before we love Him",
+                    photo: "assets/post/10.jpeg",
+                    date: "5 mins ago",
+                    userId: 10,
+                    like: 3200,
+                    comment: 9000,
+          },
+
+]
