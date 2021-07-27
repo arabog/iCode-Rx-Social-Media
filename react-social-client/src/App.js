41a063607d75d8b09b8e68f1f7b05f8bb07cb7d8
iCode_Rx <arababs2015@gmail.com>
2021-07-27 23:57:55 +0100

    profile commit

--- react-social-client/src/App.js
+++ react-social-client/src/App.js
@@ -1,10 +1,11 @@
 import Home from "./pages/home/Home"
+import Profile from "./pages/profile/Profile"
 // import {Person} from '@material-ui/icons'
 function App() {
 	return (
 		<div>
-			<Home />
-			{/* <Person /> */}
+			{/* <Home /> */}
+			<Profile />
 		</div>
 	);
 }
