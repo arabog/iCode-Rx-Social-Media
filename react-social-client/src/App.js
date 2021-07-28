f2f7da79f08f11eb300e0638d57ea5bc33b3d209
iCode_Rx <arababs2015@gmail.com>
2021-07-28 08:59:16 +0100

    login pg updates

--- react-social-client/src/App.js
+++ react-social-client/src/App.js
@@ -1,11 +1,14 @@
-import Home from "./pages/home/Home"
-import Profile from "./pages/profile/Profile"
+// import Home from "./pages/home/Home"
 // import {Person} from '@material-ui/icons'
+// import Profile from "./pages/profile/Profile"
+import Login from "./pages/login/Login"
+
 function App() {
 	return (
 		<div>
 			{/* <Home /> */}
-			<Profile />
+			{/* <Profile /> */}
+			<Login />
 		</div>
 	);
 }
