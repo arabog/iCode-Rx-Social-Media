9b48b5fe462dc2508b1f680197f587b6c4ba75ce
iCode_Rx <arababs2015@gmail.com>
2021-07-28 09:26:39 +0100

    registr pg updated

--- react-social-client/src/App.js
+++ react-social-client/src/App.js
@@ -1,16 +1,9 @@
-// import Home from "./pages/home/Home"
-// import {Person} from '@material-ui/icons'
-// import Profile from "./pages/profile/Profile"
-// import Login from "./pages/login/Login"
-import Register from "./pages/register/Register"
+import Home from "./pages/home/Home"
 
 function App() {
 	return (
 		<div>
-			{/* <Home /> */}
-			{/* <Profile /> */}
-			{/* <Login /> */}
-			<Register />
+			<Home />
 		</div>
 	);
 }
--- react-social-client/src/components/rightbar/Rightbar.jsx
+++ react-social-client/src/components/rightbar/Rightbar.jsx
@@ -93,9 +93,13 @@ export default function Rightbar({ profile }) {
 
           return (
                     <div className= "rightbar">
+
                               <div className="rightbarWrapper">
-                                        <ProfileRightbar />
+                                        {
+                                                  profile ? <ProfileRightbar /> : <HomeRightbar />
+                                        }
                               </div>
+                              
                     </div>
           ) 
 }
