16bf61aa41119fac63674aeefdf4128d5d409010
iCode_Rx <arababs2015@gmail.com>
2021-08-02 12:08:13 +0100

    more update

--- react-social-client/src/pages/profile/Profile.jsx
+++ react-social-client/src/pages/profile/Profile.jsx
@@ -5,6 +5,8 @@ import Feed from "../../components/feed/Feed"
 import Rightbar from "../../components/rightbar/Rightbar"
 
 export default function Profile() {
+          const PF = process.env.REACT_APP_PUBLIC_FOLDER
+
           return (
                     <>
                               <Topbar />
@@ -17,8 +19,8 @@ export default function Profile() {
                                                             <div className="profileRightTop">
 
                                                                       <div className="profileCover">
-                                                                                <img src="/assets/post/3.jpeg" alt="" className="profileCoverImg" />
-                                                                                <img src="/assets/post/7.jpeg" alt="" className="profileUserImg" />
+                                                                                <img src={`${PF}post/3.jpeg`} alt="" className="profileCoverImg" />
+                                                                                <img src={`${PF}person/7.jpeg`} alt="" className="profileUserImg" />
                                                                       </div>
 
                                                                       <div className="profileInfo">
