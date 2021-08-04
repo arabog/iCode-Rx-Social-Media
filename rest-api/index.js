c5d644f9c3f345d2d0f64a6ed4fbbce5e2e7e7f1
iCode_Rx <arababs2015@gmail.com>
2021-08-04 12:29:53 +0100

    public folder added

--- rest-api/index.js
+++ rest-api/index.js
@@ -18,6 +18,8 @@ const postRoute = require("./routes/posts")
 // for img
 const multer =require("multer")
 
+const path = require("path")
+
 
 dotenv.config();
 
@@ -32,10 +34,31 @@ mongoose
           .then(() => console.log("Connected to MongoDB"))
           .catch(err => console.error(err))
 
+app.use("/images", express.static(path.join(__dirname, "public/images")))
+
 // middleware
 app.use(express.json())
 app.use(helmet())
 app.use(morgan("common"))
+
+const storage = multer.diskStorage({
+          destination: (req, file, cb) => {
+                    cb(null, "public/images")
+          },
+
+          filename: (req, file, cb) => {
+                    cb(null, file.originalname)
+          }
+})
+
+const upload = multer( { storage } )
+app.post("/api/upload", upload.single("file"), (req, res) => {
+          try {
+                    return res.status(200).json("File uploaded successfully")
+          } catch (err) {
+                    console.log(err);
+          }
+})
 
 app.use('/api/users', userRoute)
 app.use('/api/auth', authRoute)
